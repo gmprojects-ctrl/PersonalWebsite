e9263e47aed3efae7dd80f2a1e046d96914d1d65
gmprojects-ctrl <68788400+gmprojects-ctrl@users.noreply.github.com>
2026-05-28 20:34:37 +0100

    Finished Website

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,3 +1,6 @@
 // Constants used across the site
 export const NAME: string = "Gihan Mallikarachchige";
 export const CURRENT_YEAR: number = new Date().getFullYear();
+export const EMAIL : string = "gihanmallik@outlook.com";
+export const LINKEDIN_URL : string = "https://www.linkedin.com/in/gmallik/";
+export const GITHUB_URL : string = "https://github.com/gmprojects-ctrl";
